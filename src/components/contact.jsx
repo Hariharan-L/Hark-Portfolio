edfebfd690485e9266349d83a4a78950328e7497
Hariharan-L <hariharanhark7@gmail.com>
2024-03-06 11:44:38 +0530

    contact updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import "../css/contact.css";
-// import resume from "../assets/Hark's_Resume.pdf";
 const Contact=()=>{
   return(
-    <div>
+    <div className="contact-page">
       <Fade triggerOnce="true" duration={3000}>
         <div id="contact" className="Contact-title">Contact</div>
       </Fade>
-      <a href="Hark's_Resume.pdf" download="true.pdf">hello</a>
-      <Fade delay={500} triggerOnce="true" duration={2000}>
+      <Fade delay={1000} triggerOnce="true" duration={2000}>
+        <h5 className="information">This is all about my portfolio thank you for visiting💫</h5>
+        <a className="Resume" href="Hark's_Resume.pdf" download="Hark's_Resume.pdf">Downlod Resume</a>
+        <div className="e-mail">hariharanhark7@gmail.com</div>
+      </Fade>
+      <Fade delay={1000} triggerOnce="true" duration={2000}>
         <div className="neon-mist7"></div>
       </Fade>
+      {/* <Fade delay={1000} triggerOnce="true" duration={2000}>
+        <div className="e-mail">hariharanhark7@gmail.com</div>
+      </Fade> */}
     </div>
   )
 }
