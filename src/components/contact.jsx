7048d23616127d2d27217d8e6262e457652be947
Hariharan-L <hariharanhark7@gmail.com>
2024-03-13 14:33:05 +0530

    contact updated

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,20 +4,15 @@ import "../css/contact.css";
 const Contact=()=>{
   return(
     <div className="contact-page">
-      <Fade triggerOnce="true" duration={3000}>
+      <Fade direction="down" triggerOnce="true" duration={3000}>
         <div id="contact" className="Contact-title">Contact</div>
       </Fade>
-      <Fade delay={1000} triggerOnce="true" duration={2000}>
         <h5 className="information">This is all about my portfolio thank you for visiting💫</h5>
         <a className="Resume" href="Hark's_Resume.pdf" download="Hark's_Resume.pdf">Downlod Resume</a>
         <div className="e-mail">hariharanhark7@gmail.com</div>
-      </Fade>
       <Fade delay={1000} triggerOnce="true" duration={2000}>
         <div className="neon-mist7"></div>
       </Fade>
-      {/* <Fade delay={1000} triggerOnce="true" duration={2000}>
-        <div className="e-mail">hariharanhark7@gmail.com</div>
-      </Fade> */}
     </div>
   )
 }
